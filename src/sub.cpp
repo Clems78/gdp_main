747da9e9f243ab8604e00b02d2c516bfef10bf4f
“Clems78” <clems.laguerre@free.fr>
2024-03-07 15:57:33 +0000

    distance between center of the frame and center of the bounding box is done

--- src/sub.cpp
+++ src/sub.cpp
@@ -1,14 +1,50 @@
 #include <ros/ros.h>
 #include <darknet_ros_msgs/BoundingBoxes.h>
+#include <tuple>
 
 using namespace std;
 
-void detection_cb(const darknet_ros_msgs::BoundingBoxes::ConstPtr& msg)
+//Image width and height
+int width = 640;
+int height = 480;
+int img_center_x = width/2;
+int img_center_y = height/2;
+
+void detection_cb(const darknet_ros_msgs::BoundingBoxes::ConstPtr& msg) // callback function
 {
 
 	for (int i=0; i<msg->bounding_boxes.size(); i++ ) // run on each bouding box that is in the message
 	{
-		ROS_INFO("%s detected", msg->bounding_boxes[i].Class.c_str()); //convert into a char array that can be used by ros
+		ROS_INFO("%s detected", msg->bounding_boxes[i].Class.c_str()); //convert into a char array that can be used by ros; print detected object
+
+		string boxe_name = msg->bounding_boxes[i].Class.c_str();
+
+		if (boxe_name == "person") // condition should be changed to "rccars" We could also restrict the detection of Yolo to only rc cars and remove all the other classes to prevent issues
+		{
+			//Extract the coordinates of the boxes
+			int xMin = msg->bounding_boxes[i].xmin;
+			int xMax = msg->bounding_boxes[i].xmax;
+			int yMin = msg->bounding_boxes[i].ymin;
+			int yMax = msg->bounding_boxes[i].ymax;
+
+			//Print information
+			ROS_INFO("Boxe number %ld", i);
+			ROS_INFO("xmin: %ld", xMin);
+			ROS_INFO("xmax: %ld", xMax);
+			ROS_INFO("ymin: %ld", yMin);
+			ROS_INFO("ymax: %ld", yMax);
+			ROS_INFO("id: %ld", msg->bounding_boxes[i].id);
+
+			//Get the center 
+			float bb_center_x = xMin + (xMax - xMin) / 2;
+			float bb_center_y = yMin + (yMax - yMin) / 2;
+			ROS_INFO("Bounding Box center coordinates: (%lf, %lf)", bb_center_x, bb_center_y);
+
+			//Compute distance to image center
+			float dist2center_x = bb_center_x - img_center_x;
+			float dist2center_y = bb_center_y - img_center_y;
+			ROS_INFO("Center is %lf pixels away in x and %lf pixels away in y", dist2center_x, dist2center_y);
+		}
 	}
 }
 
