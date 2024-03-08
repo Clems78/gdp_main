56dc7268cc8e54a520ee94ea34a66416bbe7ed28
“Clems78” <clems.laguerre@free.fr>
2024-03-08 13:50:47 +0000

    add folder for flight arena test in real life - detect and land mission ready

--- src/Flight_Arena_Test/search_detect_land_fl.cpp
+++ src/Flight_Arena_Test/search_detect_land_fl.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <darknet_ros_msgs/BoundingBoxes.h>
 #include <geometry_msgs/Point.h>
+#include <gnc_functions.hpp>
 
 using namespace std;
 
@@ -10,20 +11,21 @@ int height = 480;
 int img_center_x = width/2;
 int img_center_y = height/2;
 
-//Pixel to meters
-float p2m = 0.01;
+int mode = 0;
 
 void detection_cb(const darknet_ros_msgs::BoundingBoxes::ConstPtr& msg) // callback function
 {
-
 	for (int i=0; i<msg->bounding_boxes.size(); i++ ) // run on each bouding box that is in the message
 	{
 		ROS_INFO("%s detected", msg->bounding_boxes[i].Class.c_str()); //convert into a char array that can be used by ros; print detected object
 
 		string boxe_name = msg->bounding_boxes[i].Class.c_str();
 
-		if (boxe_name == "chair") // condition should be changed to "rccars" We could also restrict the detection of Yolo to only rc cars and remove all the other classes to prevent issues
+		if (boxe_name == "truck") // condition should be changed to "rccars" We could also restrict the detection of Yolo to only rc cars and remove all the other classes to prevent issues
 		{
+			ROS_INFO("%s detected", msg->bounding_boxes[i].Class.c_str());
+			mode = 1;
+
 			//Extract the coordinates of the boxes
 			int xMin = msg->bounding_boxes[i].xmin;
 			int xMax = msg->bounding_boxes[i].xmax;
@@ -48,24 +50,9 @@ void detection_cb(const darknet_ros_msgs::BoundingBoxes::ConstPtr& msg) // callb
 			float pix_dist2center_y = bb_center_y - img_center_y;
 			ROS_INFO("Center is %lf pixels away in x and %lf pixels away in y", pix_dist2center_x, pix_dist2center_y);
 
-			//Convert into meters
-			float dist2center_x = pix_dist2center_x * p2m;
-			float dist2center_y = pix_dist2center_y * p2m;
-
-			//Detail the message content
-			geometry_msgs::Point dist2center; //Give a name to the message chosen in the Publisher line 
-			dist2center.x = dist2center_x; // Assign value to the first data of the message
-			dist2center.y = dist2center_y; // Assign value to the second data of the message
-			dist2center.z = 0; // Assign value to the third data of the message
-
-
-			//Publish the message
-			//dist_pub.publish(dist2center);
-
 		}
 	}
 }
-
 
 int main(int argc, char **argv) {
 
@@ -75,15 +62,57 @@ int main(int argc, char **argv) {
 	//	ros::Subscriber sub = <nodehandle>.subscribe("<topic>", <# of msg buffered>, <name of callback function>);
 	ros::Subscriber sub = n.subscribe("/darknet_ros/bounding_boxes", 1, detection_cb); //1 = how many message buffered. default 1
 
-	// ros::Publisher pub = n.advertise<std_msgs::message-type>("topic_name", 5);
-	ros::Publisher dist_pub = n.advertise<geometry_msgs::Point>("distance_to_bb_center", 1000); //1000 is the queue size
+	//initialize control publisher/subscribers
+	init_publisher_subscriber(n);
 
-	// Publish at 10Hz
-	ros::Rate loop_rate(10);
+  	// wait for FCU connection
+	wait4connect();
 
+	//Wait for user to set mode to guided
+	wait4start();
 
-	ros::spin(); //makes the int main run infinitely
+	//create local reference frame 
+	initialize_local_frame();
 
+	//request takeoff
+	takeoff(1.2);
+
+	//specify some waypoints 
+	std::vector<gnc_api_waypoint> waypointList;
+	gnc_api_waypoint nextWayPoint;
+	nextWayPoint.x = 0;
+	nextWayPoint.y = 0;
+	nextWayPoint.z = 1.2;
+	nextWayPoint.psi = 0;
+	waypointList.push_back(nextWayPoint);
+
+	//specify control loop rate. We recommend a low frequency to not over load the FCU with messages. Too many messages will cause the drone to be sluggish
+	ros::Rate rate(2.0); // loop execution rate
+	int counter = 0;
+	while(ros::ok()) // loop as long as the node is running
+	{
+		if (mode == 0)
+		{
+			ros::spinOnce(); // Allows to continue processing main loop but still using callback information
+			rate.sleep(); // Ensure ros rate 
+			if(check_waypoint_reached(.3) == 1)
+			{
+				if (counter < waypointList.size())
+				{
+					set_destination(waypointList[counter].x,waypointList[counter].y,waypointList[counter].z, waypointList[counter].psi);
+					counter++;	
+				}
+			}	
+		}
+
+		if (mode == 1)
+		{
+			ROS_INFO("LANDING");
+			land();
+			break; // finish the program
+		}
+		
+	}
 
 return 0;
 }
