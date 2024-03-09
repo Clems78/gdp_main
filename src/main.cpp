a5f8f970eee87d351e27f8d40e9605dcf63aedde
“Clems78” <clems.laguerre@free.fr>
2024-03-09 13:57:34 +0000

    tracked vehicle coordinate published when tracking is done

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <ros/ros.h>
 #include <darknet_ros_msgs/BoundingBoxes.h>
 #include <gnc_functions.hpp>
+#include <sensor_msgs/NavSatFix.h>
+#include <geometry_msgs/Point.h>
 
 using namespace std;
 
@@ -21,7 +23,7 @@ float dist2center_y = 0;
 int mode = 0;
 
 //Flying altitude
-float alt = 3;
+float target_alt = 3;
 
 // Time after which the drone goes back to searching if the target is lost during tracking 
 int lost_target_time = 5;
@@ -38,27 +40,50 @@ int tracking_counter = 0;
 //Condition on first tracking waypoint
 int first_tracking_waypoint = 0;
 
-void detection_cb(const darknet_ros_msgs::BoundingBoxes::ConstPtr& msg) // callback function
+//Publisher//
+//tracked vehicle coordinate publisher 
+ros::Publisher tracked_vehicle_pos_pub;
+
+//Initialise tracked vehicle coordinate message
+geometry_msgs::Point position_msg;
+
+//Get global coordinates of the drone
+void pos_cb(const sensor_msgs::NavSatFix::ConstPtr& pos_msg)
+{
+	//Extract data
+	float latitude = pos_msg->latitude;
+	float longitude = pos_msg->longitude;
+	float altitude = pos_msg->altitude;
+	
+
+	//Detail message content
+	position_msg.x = latitude;
+    position_msg.y = longitude;
+    position_msg.z = altitude;
+}
+
+//const <msg package name>::<message>::ConstPtr& msg
+void darknet_cb(const darknet_ros_msgs::BoundingBoxes::ConstPtr& darknet_msg) // callback function
 {
 	ROS_INFO("mode :  %ld", mode);
 
-	for (int i=0; i<msg->bounding_boxes.size(); i++ ) // run on each bouding box that is in the message
+	for (int i=0; i<darknet_msg->bounding_boxes.size(); i++ ) // run on each bouding box that is in the message
 	{
-		ROS_INFO("%s detected", msg->bounding_boxes[i].Class.c_str()); //convert into a char array that can be used by ros; print detected object
+		ROS_INFO("%s detected", darknet_msg->bounding_boxes[i].Class.c_str()); //convert into a char array that can be used by ros; print detected object
 
-		string boxe_name = msg->bounding_boxes[i].Class.c_str();
+		string boxe_name = darknet_msg->bounding_boxes[i].Class.c_str();
 
 		if (boxe_name == "person") // condition should be changed to "rccars" We could also restrict the detection of Yolo to only rc cars and remove all the other classes to prevent issues
 		{
-			ROS_INFO("%s detected", msg->bounding_boxes[i].Class.c_str());
+			ROS_INFO("%s detected", darknet_msg->bounding_boxes[i].Class.c_str());
 			mode = 1;
 			target_lost_counter = 0; // initialise or put back target lost counter to 0
 
 			//Extract the coordinates of the boxes
-			int xMin = msg->bounding_boxes[i].xmin;
-			int xMax = msg->bounding_boxes[i].xmax;
-			int yMin = msg->bounding_boxes[i].ymin;
-			int yMax = msg->bounding_boxes[i].ymax;
+			int xMin = darknet_msg->bounding_boxes[i].xmin;
+			int xMax = darknet_msg->bounding_boxes[i].xmax;
+			int yMin = darknet_msg->bounding_boxes[i].ymin;
+			int yMax = darknet_msg->bounding_boxes[i].ymax;
 
 			//Print information
 			ROS_INFO("Boxe number %ld", i);
@@ -66,7 +91,7 @@ void detection_cb(const darknet_ros_msgs::BoundingBoxes::ConstPtr& msg) // callb
 			ROS_INFO("xmax: %ld", xMax);
 			ROS_INFO("ymin: %ld", yMin);
 			ROS_INFO("ymax: %ld", yMax);
-			ROS_INFO("id: %ld", msg->bounding_boxes[i].id);
+			ROS_INFO("id: %ld", darknet_msg->bounding_boxes[i].id);
 
 			//Get the center 
 			float bb_center_x = xMin + (xMax - xMin) / 2;
@@ -110,11 +135,19 @@ void detection_cb(const darknet_ros_msgs::BoundingBoxes::ConstPtr& msg) // callb
 // Main function with argument from the launch file 
 int main(int argc, char **argv) {
 
-	ros::init(argc, argv, "detection_sub"); //name of the node
+	ros::init(argc, argv, "main"); //name of the node
 	ros::NodeHandle n; //enable connection to the ros network
 
 	//	ros::Subscriber sub = <nodehandle>.subscribe("<topic>", <# of msg buffered>, <name of callback function>);
-	ros::Subscriber sub = n.subscribe("/darknet_ros/bounding_boxes", 1, detection_cb); //1 = how many message buffered. default 1
+	ros::Subscriber darknet_sub = n.subscribe("/darknet_ros/bounding_boxes", 1, darknet_cb); //1 = how many message buffered. default 1
+
+	//Getting global coordinate
+	ros::Subscriber pos_sub = n.subscribe("/mavros/global_position/global", 1, pos_cb);
+
+	// ros::Publisher pub = n.advertise<std_msgs::message-type>("created topic_name", 5);
+	// Publish the position of the tracked vechile on another node
+	tracked_vehicle_pos_pub = n.advertise<geometry_msgs::Point>("tracked_vehicle_pos", 1); //1000 is the queue size
+
 
 	//initialize control publisher/subscribers
 	init_publisher_subscriber(n);
@@ -129,34 +162,34 @@ int main(int argc, char **argv) {
 	initialize_local_frame();
 
 	//request takeoff
-	takeoff(alt);
+	takeoff(target_alt);
 
 	//specify some waypoints 
 	std::vector<gnc_api_waypoint> waypointList;
 	gnc_api_waypoint nextWayPoint;
 	nextWayPoint.x = 0;
 	nextWayPoint.y = 0;
-	nextWayPoint.z = alt;
+	nextWayPoint.z = target_alt;
 	nextWayPoint.psi = 0;
 	waypointList.push_back(nextWayPoint);
 	nextWayPoint.x = 5;
 	nextWayPoint.y = 0;
-	nextWayPoint.z = alt;
+	nextWayPoint.z = target_alt;
 	nextWayPoint.psi = 0;
 	waypointList.push_back(nextWayPoint);
 	nextWayPoint.x = 5;
 	nextWayPoint.y = 5;
-	nextWayPoint.z = alt;
+	nextWayPoint.z = target_alt;
 	nextWayPoint.psi = 0;
 	waypointList.push_back(nextWayPoint);
 	nextWayPoint.x = 0;
 	nextWayPoint.y = 5;
-	nextWayPoint.z = alt;
+	nextWayPoint.z = target_alt;
 	nextWayPoint.psi = 0;
 	waypointList.push_back(nextWayPoint);
 	nextWayPoint.x = 0;
 	nextWayPoint.y = 0;
-	nextWayPoint.z = alt;
+	nextWayPoint.z = target_alt;
 	nextWayPoint.psi = 0;
 	waypointList.push_back(nextWayPoint);
 
@@ -165,13 +198,12 @@ int main(int argc, char **argv) {
 	int counter = 0;
 	while(ros::ok()) // loop as long as the node is running
 	{	
-		ROS_INFO("mode :  %ld", mode);
-
 		if (mode == 0) //SEARCHING MODE
-		{
+		{	
+			ros::spinOnce(); // Allows to continue processing main loop but still using callback information
+			ROS_INFO("#####SEARCHING#####SEARCHING#####SEARCHING#####");
 			tracking_counter = 0;
 			first_tracking_waypoint = 0;
-			ros::spinOnce(); // Allows to continue processing main loop but still using callback information
 			rate.sleep(); // Ensure ros rate 
 			if(check_waypoint_reached(.3) == 1)
 			{
@@ -186,23 +218,23 @@ int main(int argc, char **argv) {
 		else if (mode == 1) //TRACKING MODE
 		{
 			ros::spinOnce();
-			ROS_INFO("####TRACKING#####TRACKING#####TRACKING#########"); 
+			ROS_INFO("#####TRACKING#####TRACKING#####TRACKING#####"); 
 			geometry_msgs:: Point current_pos;
 			current_pos = get_current_location();
 
 			if(first_tracking_waypoint == 0)
 			{
 				ROS_INFO("Current position : %lf, %lf", current_pos.x, current_pos.y);
-				set_destination(current_pos.x + dist2center_x, current_pos.y - dist2center_y, alt, 0);
+				set_destination(current_pos.x + dist2center_x, current_pos.y - dist2center_y, target_alt, 0);
 				first_tracking_waypoint++;
 				ROS_INFO("Moving to first tracking point");
 			}else if(check_waypoint_reached(.3) == 1){
-				set_destination(current_pos.x + dist2center_x, current_pos.y + dist2center_y, alt, 0);
+				set_destination(current_pos.x + dist2center_x, current_pos.y + dist2center_y, target_alt, 0);
 				ROS_INFO("Moving to next tracking point");
 			}
 
 			ROS_INFO("tracking counter before the loop %ld", tracking_counter);
-			ros::Rate tracking_rate(2.0);
+			ros::Rate tracking_rate(1.0);
 			if (tracking_counter < tracking_time){
 				while(ros::ok())
 				{	ROS_INFO("tracking time %ld", tracking_time);
@@ -214,8 +246,10 @@ int main(int argc, char **argv) {
 				}
 			}else{
 				ROS_INFO("Tracking done ! Back to searching !");
-				//Add coordinate of the vehicle ++ add condition on the tracking
-				// Publish 
+				// Publish to the topic of tracked vehicle coordinates
+				tracked_vehicle_pos_pub.publish(position_msg);
+    			ROS_INFO("latitude is : %lf", position_msg.x);
+				// Publish to the topic number of tracked vehicle 
 				mode = 0;
 			}
 		}	
